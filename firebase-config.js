9254cc3acfc0bc1abd9e52dbea68af4e91f66a98
kemomashakel251-tech <kemomashakel251@gmail.com>
2026-08-07 22:36:04 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,13 @@
 // هات القيم دي من: Firebase Console > Project settings > General > Your apps > SDK setup
 // =====================================================================
 const firebaseConfig = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyBefkh7GpID57Mhir8JPoFdCoDOzY2Zv20",
+  authDomain: "mrtt-564b6.firebaseapp.com",
+  projectId: "mrtt-564b6",
+  storageBucket: "mrtt-564b6.firebasestorage.app",
+  messagingSenderId: "19184284333",
+  appId: "1:19184284333:web:735a549e7c36ffdbdfcc24",
+  measurementId: "G-KCHL22BF10"
 };
 
 firebase.initializeApp(firebaseConfig);
